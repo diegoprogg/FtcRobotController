aa2ebed698d61e94868e37e42479a4e426cc564d
diegoprogg <diegocarlos2307@gmail.com>
2026-02-18 10:58:54 -0300

    Create FirstCodeClass.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstCodeClass.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstCodeClass extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
